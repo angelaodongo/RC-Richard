80d7ffa12900fe126967920405bfecb6014953b5
Angela Odongo <celesteodongo@gmail.com>
2025-01-04 14:16:05 +0300

    created the side panel

---
+++ script.js
@@ -0,0 +1,9 @@
+function openPanel(){
+    document.getElementById("sidePanel").style.width = "250px";
+    document.querySelector(".container").style.marginleft = "250px";
+}
+
+function closePanel(){
+    document.getElementById("sidePanel").style.width = "0";
+    document.querySelector(".container").style.marginleft = "0";
+}
